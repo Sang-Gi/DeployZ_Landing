05e7205f0496bdcdb3c20dc0478ce40648893c73
aldls819 <aldls819@gmail.com>
2023-05-16 19:34:19 +0900

    :hammer: Fix: 프로젝트 충돌 해결

--- frontend/src/components/InstallGuide/DeployzInstall.tsx
+++ frontend/src/components/InstallGuide/DeployzInstall.tsx
@@ -1,15 +1,19 @@
 import styled from "styled-components";
 import { theme } from "@/styles/theme";
-import { styled as mstyled } from "@mui/material/styles";
-import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import "react-vertical-timeline-component/style.min.css";
+import { VerticalTimelineElement } from "react-vertical-timeline-component";
+import { RiNumber1, RiNumber2 } from "react-icons/ri";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import { styled as mstyled } from "@mui/material/styles";
 import { CopyToClipboard } from "react-copy-to-clipboard/src";
 import { info } from "@components/common/Toast/notify";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 
 export default function DeployzInstall() {
   return (
     <>
-      <Container>
+      {/* <Container></Container> */}
+      <SInstallGuideNameDiv>
         <STitleDiv>
           <CloudUpload />
           DeployZ 설치
@@ -17,58 +21,34 @@ export default function DeployzInstall() {
         <SDescDiv>
           <SDescP>DeployZ를 설치하고 자동배포하기</SDescP>
         </SDescDiv>
-        <div
-          style={{
-            width: "100%",
-            textAlign: "center",
-            borderBottom: "1px solid #aaa",
-            lineHeight: "0.1em",
-            margin: "10px 0 20px",
-          }}
-        />
-        <Bottomdiv>
-          <CopyToClipboard
-            className="Toram"
-            text="https://hub.docker.com/"
-            onCopy={() => info("복사되었습니다.")}
-          >
-            <ColorContainer>
-              <text>https://hub.docker.com/</text>
-              <CopyIcon />
-            </ColorContainer>
-          </CopyToClipboard>
-        </Bottomdiv>
-        <div
-          style={{
-            width: "100%",
-            textAlign: "center",
-            borderBottom: "1px solid #aaa",
-            lineHeight: "0.1em",
-            margin: "2rem 0",
-          }}
-        />
-      </Container>
+      </SInstallGuideNameDiv>
+      <Contentdiv>
+        <CopyToClipboard
+          className="Toram"
+          text="sudo systemctl stop nginx"
+          onCopy={() => info("복사되었습니다.")}
+        >
+          <ColorContainer>
+            <text>https://hub.docker.com/</text>
+            <CopyIcon />
+          </ColorContainer>
+        </CopyToClipboard>
+      </Contentdiv>
+      <Contentdiv>
+        <CopyToClipboard
+          className="Toram"
+          text="sudo systemctl stop nginx"
+          onCopy={() => info("복사되었습니다.")}
+        >
+          <ColorContainer>
+            <text>https://hub.docker.com/</text>
+            <CopyIcon />
+          </ColorContainer>
+        </CopyToClipboard>
+      </Contentdiv>
     </>
   );
 }
-const CopyIcon = mstyled(ContentCopyIcon)({
-  marginLeft: "1rem",
-  color: `${theme.colors.primary}`,
-  "&:hover": {
-    color: `${theme.colors.secondary}`,
-    transition: "all .2s ease-out",
-    transform: "scale(1.3)",
-    cursor: "pointer",
-  },
-});
-const ColorContainer = styled.div`
-  background-color: ${theme.colors.lightgray};
-  font-weight: ${theme.fontWeight.semibold};
-  font-size: 1.5rem;
-  padding: 1rem;
-  border-radius: 1rem;
-  margin-top: 1rem;
-`;
 const CloudUpload = mstyled(CloudUploadIcon)({
   fontSize: "6.5rem",
   color: theme.colors.primary,
@@ -90,26 +70,47 @@ const SDescDiv = styled.div`
   align-items: center;
   text-align: center;
   flex-direction: column;
-  padding-bottom: 2rem;
 `;
 
-const Container = styled.div`
+const SInstallGuideNameDiv = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  width: 80%;
+  position: relative;
+  top: -1rem;
 `;
 
 const STitleDiv = styled.div`
-  font-size: 3.5rem;
+  font-size: 4.5rem;
   color: ${theme.colors.primary};
-  font-weight: ${theme.fontWeight.extrabold};
+  font-weight: ${theme.fontWeight.semibold};
 `;
 
-const Bottomdiv = styled.div`
+const CopyIcon = mstyled(ContentCopyIcon)({
+  marginLeft: "1rem",
+  color: `${theme.colors.primary}`,
+  "&:hover": {
+    color: `${theme.colors.secondary}`,
+    transition: "all .2s ease-out",
+    transform: "scale(1.3)",
+    cursor: "pointer",
+  },
+});
+
+const Contentdiv = styled.div`
   display: flex;
   justify-content: center;
-  align-items: center;
-  padding: 2rem;
+  align-items: start;
+  flex-direction: column;
+  padding: 0;
+`;
+
+const ColorContainer = styled.div`
+  background-color: ${theme.colors.lightgray};
+  font-weight: ${theme.fontWeight.semibold};
+  font-size: 1.5rem;
+  padding: 1rem;
+  border-radius: 1rem;
+  margin-top: 1rem;
 `;
